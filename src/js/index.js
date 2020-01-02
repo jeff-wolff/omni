bac19fa8d25eec6f795170ff1931b92642a6bb22
jeff <xhilacom@gmail.com>
2020-01-01 19:55:54 -0800

    size units and breakpoints

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,8 @@
 import '../css/omni.css';
 // import { writeToScreen } from './text'
 // writeToScreen()
+let range = "";
+for (let index = 4; index < 121; index+=4) {
+  range += index+', ';
+}
+console.log(range);
