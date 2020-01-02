e85e29382ee61270d168c0fbe729d8939493c7d2
jeff <xhilacom@gmail.com>
2020-01-01 20:08:53 -0800

    tidy up

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,3 @@
 import '../css/omni.css';
 // import { writeToScreen } from './text'
 // writeToScreen()
-let range = "";
-for (let index = 4; index < 121; index+=4) {
-  range += index+', ';
-}
-console.log(range);
